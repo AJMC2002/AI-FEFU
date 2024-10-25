7747f14af8b75b58843418915b327a0185f6477b
AJMC2002 <adrianmosquera2015@gmail.com>
2024-10-26 01:08:39 +1000

    Removed old files

--- dz-dataset/migrations/2024-10-12-030032_create_poems/up.sql
+++
@@ -1,9 +0,0 @@
--- Your SQL goes here
-CREATE TABLE "poems"(
-	"id" SERIAL NOT NULL PRIMARY KEY,
-	"author" VARCHAR,
-	"title" VARCHAR NOT NULL,
-	"body" TEXT NOT NULL,
-	"topic" VARCHAR NOT NULL
-);
-
